2422495c22802be9ce9831db7e44e7049da930fc
Devin McKinney <devin.mckinney02@gmail.com>
2025-01-24 15:14:35 -0700

    Ensure all required variables are set in terraform.tfvars and GitHub Actions workflow

--- dns.tf
+++ dns.tf
@@ -1,23 +1,3 @@
-/*
-This Terraform configuration file defines resources for managing DNS and routing within an Azure environment. The resources include:
-
-1. Private DNS Zone:
-   - azurerm_private_dns_zone.example: Creates a private DNS zone named `fusion.private` within the specified resource group.
-
-2. Private DNS Zone Virtual Network Links:
-   - azurerm_private_dns_zone_virtual_network_link.example_link_vnet1: Links the private DNS zone to the virtual network specified by `module.vnet.vnet_id`.
-   - azurerm_private_dns_zone_virtual_network_link.example_link_vnet2: Links the private DNS zone to the on-premises virtual network specified by `azurerm_virtual_network.onprem.id`.
-
-3. Private DNS A Record:
-   - azurerm_private_dns_a_record.example_record: Creates an A record named `fusion-record` within the private DNS zone, pointing to the IP address `10.0.1.10`.
-
-4. Route Table:
-   - azurerm_route_table.example: Creates a route table named `example-route-table` within the specified resource group and location. It includes a route named `route-to-vnet2` that directs traffic to the address space `10.1.0.0/16` via a virtual network gateway.
-
-5. Subnet Route Table Association:
-   - azurerm_subnet_route_table_association.example: Associates the route table with the `webapp` subnet in the virtual network.
-*/
-
 resource "azurerm_private_dns_zone" "example" {
   name                = "fusion.private"
   resource_group_name = azurerm_resource_group.main.name
