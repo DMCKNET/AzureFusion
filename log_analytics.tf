d7f175d64f54acfa2f39b1c4085b13b9e9921226
Devin McKinney <devin.mckinney02@gmail.com>
2025-01-24 15:57:14 -0700

    Ensure all required variables are set in terraform.tfvars and GitHub Actions workflow

--- log_analytics.tf
+++ log_analytics.tf
@@ -1,3 +1,6 @@
+/*
+This Terraform configuration file defines a resource for managing an Azure Log Analytics Workspace.
+*/
 resource "azurerm_log_analytics_workspace" "main" {
   name                = "${var.prefix}-log-analytics"
   location            = var.location
